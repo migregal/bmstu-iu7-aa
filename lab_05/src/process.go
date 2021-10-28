44f7d627fffca1c36973575b0a7695a729cd1f89
migregal <devmigregal@gmail.com>
2021-10-28 20:14:36 +0300

    Added info output for conveyer

--- lab_05/src/process.go
+++ lab_05/src/process.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"lab_05/md5conveyor"
+	"sort"
 )
 
 func process(data []md5conveyor.Output, realTime int64) {
@@ -32,4 +34,18 @@ func process(data []md5conveyor.Output, realTime int64) {
 	fmt.Println("Avg digester queue time:", float64(digesterQueue)/float64(total))
 	fmt.Println("Avg digester time:      ", float64(digester)/float64(total))
 	fmt.Println("Avg queue time:         ", float64(queue)/float64(total))
+
+	fmt.Println()
+	m := make(map[string][md5.Size]byte)
+	for _, r := range data {
+		m[r.Path] = r.Sum
+	}
+	var paths []string
+	for path := range m {
+		paths = append(paths, path)
+	}
+	sort.Strings(paths)
+	for _, path := range paths {
+		fmt.Printf("%10x  %s\n", m[path], path)
+	}
 }
